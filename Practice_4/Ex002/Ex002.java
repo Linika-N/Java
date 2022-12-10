bf300dca104410d7035123eea44c9ca8564a30d1
Linika <lidya-novikova.95@yandex.ru>
2022-12-10 13:23:20 +0300

    Ex002

--- Practice_4/Ex002/Ex002.java
+++ Practice_4/Ex002/Ex002.java
@@ -14,18 +14,17 @@ public class Ex002 {
     public static void main(String[] args) {
         LinkedList<Integer> list = createList();
         System.out.println("Первоначальный список:\n"+list);
+        Integer indexOfElement = scan();
+        enqueue(list,indexOfElement);
+        Integer firstElement = dequeue(list);
+        print(list, firstElement);
+        firstElement = first(list);
+        print(list, firstElement);
+         
     }
-    public static LinkedList<Integer> createList() {
-        Random rnd = new Random();
-        LinkedList<Integer> linkedList = new LinkedList<>();
-        for (int g = 0; g<rnd.nextInt(5,15); g++){
-            linkedList.add(rnd.nextInt(-25, 25));
-        }
-        return linkedList;
-    }
-    public static Integer userData(String msg) {
+    public static Integer scan() {
         Scanner scn = new Scanner(System.in);
-        System.out.println("msg");
+        System.out.println("Введите порядковый номер элемента для перемещения в конец очереди: ");
         Integer temp = scn.nextInt();
         while (temp<1){
             System.out.println("Число должно быть больше 0!!!");
@@ -34,23 +33,33 @@ public class Ex002 {
         scn.close();
         return temp;
     }
-    
+    public static void print(LinkedList<Integer> startList, Integer element) {
+        System.out.printf("Из списка удален первый элемент: %d\n", element);
+        System.out.println(startList); 
+    }
+    public static LinkedList<Integer> createList() {
+        Random rnd = new Random();
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for (int g = 0; g<rnd.nextInt(5,15); g++){
+            linkedList.add(rnd.nextInt(-25, 25));
+        }
+        return linkedList;
+    }    
     // enqueue() - помещает элемент в конец очереди
-    public static void enqueue(LinkedList<Integer> startList) {
-        int number = userData("Введите элемент для переноса в конец очереди");
-        Integer temp = startList.remove(number);
+    public static void enqueue(LinkedList<Integer> startList, int number) {
+        Integer temp = startList.remove(number-1);
         startList.addLast(temp);
-        Integer num = userData("Введите порядковый номер элемента, который хоите переместить в конец очереди");
-        
+        System.out.printf("Элемент под номером %d (%d) перенесен в конец очереди:\n",number,startList.getLast()); 
+        System.out.println(startList);       
     }
     
     // dequeue() - возвращает первый элемент из очереди и удаляет его
-    public static void dequeue(String[] args) {
-        
+    public static Integer dequeue(LinkedList<Integer> startList) {
+        return startList.removeFirst();
     }
 
     // first() - возвращает первый элемент из очереди, не удаляя.
-    public static void first(String[] args) {
-        
+    public static Integer first(LinkedList<Integer> startList) {
+        return startList.getFirst();
     }
 }
